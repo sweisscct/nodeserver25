577eab161758b88ffd99719ec7a44b65403a8878
Sam Weiss <sweiss@cct.ie>
2025-03-27 13:05:27 +0000

    HTTP Routes

--- index.js
+++ index.js
@@ -11,6 +11,28 @@ app.get("/", (req, res) => {
     res.send("Welcome to the web server!");
 })
 
+app.get("/hello", (req, res) => {
+    res.send("<h1>Hello</h1><p>How are you?</p>");
+})
+
+app.get("/html", (req, res) => {
+    res.sendFile("index.html", {root: __dirname});
+})
+
+// url parameter
+app.get("/params/:display", (req, res) => {
+    res.send(req.params.display);
+})
+
+// url query
+app.get("/query", (req, res) => {
+    res.send(`The number is: ${req.query.number} and the text is: ${req.query.text}.`);
+})
+
+// http://localhost:3000/query?number=42&text=JavaScript
+
+// https://moodle.cct.ie/course/view.php?id=1996#section-7
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
